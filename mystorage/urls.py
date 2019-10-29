03f2aa4bbd4b4c60103e273dd36003c89be15bde
saurav <hong-gildong@hong-gildong-ui-MacBookPro.local>
2019-10-29 10:22:28 +0900

    final commit

---
+++ mystorage/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from mystorage import views
+
+
+router= DefaultRouter()
+router.register('essay',views.PostViewSet)
+router.register('album',views.ImageViewSet)
+router.register('files',views.FileViewSet)
+
+urlpatterns=[
+    path('',include(router.urls))
+]
